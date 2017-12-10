a1abacc6a384726c973fb2e49f4e74d71ff0f869
Andras <andras@kinvey.com>
2017-12-10 16:35:19 -0500

    also include the protocol-buffers benchmark

--- benchmark.js
+++ benchmark.js
@@ -23,8 +23,47 @@ var data = {
 };
 var dataA = [ data.num, data.payload ];
 
+var format = 'aIIIIibbbbbbbdf';
+var schema = [
+    'message Test {',
+    '  required string str = 1;',
+    '  required uint32 iso = 2;',       // unsigned vint (but stores 'ff' for negative and breaks on decode)
+    '  required int32 date = 3;',       // unsigned vint (but stores 32 ff bits for negative ?!)
+    '  required int64 long = 4;',       // unsigned vint?? (but stores 64 ff bits for negative ?!)
+    '  required int32 typeid = 5;',
+    '  required sint32 sint = 6;',      // signed varint
+    '  required bool b1 = 7;',          // varint, 1 byte
+    '  required bool b2 = 8;',
+    '  required bool b3 = 9;',
+    '  required bool b4 = 10;',
+    '  required bool b5 = 11;',
+    '  required bool b6 = 12;',
+    '  required bool b7 = 13;',
+    '  required double doub = 14;',
+    '  required float flt = 15;',
+    '}',
+].join('\n');
+
+var data = {
+x: -1,
+    str: 'Lorem ipsum dolor sit amet.',
+    iso: 9000,
+    date: 20161110,
+    long: 649545084044315,
+    typeid: 1,
+    sint: -42,
+    b1: true, b2: false, b3: false,
+    b4: true, b5: false, b6: false, b7: true,
+    doub: 204.8,
+    flt: 0.25,
+};
+var dataA = [
+    , 'Lorem ipsum dolor sit amet.', 9000, 20161110, 649545084044315, 1, -42,
+    true, false, false, true, false, false, true, 204.8, 0.25
+];
 
 var messages = pbuf(schema);
+
 
 var json = JSON.stringify(data);
 var buf = messages.Test.encode(data);
@@ -41,14 +80,17 @@ qtimeit.bench.visualize = true;
 qtimeit.bench({
     'pbuf enc': function() { x = messages.Test.encode(data) },
     'json enc': function() { x = JSON.stringify(data) },
-    'pblite enc': function() { x = pblite.pack(format, data) },
+    'pblite pack': function() { x = pblite.pack(format, dataA) },
+    'pblite _pack': function() { x = pblite._pack(format, dataA, new Array(), {p:0}) },
+});
+console.log(JSON.stringify(x));
 
+qtimeit.bench({
     'pbuf dec': function() { x = messages.Test.decode(buf) },
     'json dec': function() { x = JSON.parse(json) },
-    'pblite dec': function() { x = pblite.unpack(format, buf) },
+    'pblite unpack': function() { x = pblite.unpack(format, buf) },
 
 });
-
 console.log(x);
 
 /*
